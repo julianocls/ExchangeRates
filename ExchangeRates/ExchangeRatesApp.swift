e1620317b8527841e5291880a4407f0edccedbeb
Juliano C L Santos <foxports@hotmail.com>
2023-05-01 00:52:36 -0300

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Juliano Santos on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
