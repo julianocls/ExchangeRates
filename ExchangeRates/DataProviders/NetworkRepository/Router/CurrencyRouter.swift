bb6314fda2aa915bd01e4d063be28e3c4039b7a8
Juliano C L Santos <foxports@hotmail.com>
2023-05-03 00:03:50 -0300

    Create router of Currency

--- ExchangeRates/DataProviders/NetworkRepository/Router/CurrencyRouter.swift
+++ ExchangeRates/DataProviders/NetworkRepository/Router/CurrencyRouter.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum CurrencyRouter {
+    
+    case symbols
+    
+    var path: String {
+        switch self {
+        case .symbols:
+            return RatesApi.symbols
+        }
+    }
+    
+    func asUrlRequest() throws -> URLRequest? {
+        guard var url = URL(string: RatesApi.baseUrl) else { return nil }
+        
+        switch self {
+        case .symbols:
+            var request = URLRequest(url: url.appendingPathComponent(path), timeoutInterval: Double.infinity)
+            request.httpMethod = HttpMethod.get.rawValue
+            request.addValue(RatesApi.apiKey, forHTTPHeaderField: "apikey")
+            return request
+        }
+    }
+    
+}
