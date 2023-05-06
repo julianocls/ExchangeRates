e806343910ce3252f26b18200e70d0f2bb8d2826
Juliano C L Santos <foxports@hotmail.com>
2023-05-06 00:24:07 -0300

    fix code smell

--- ExchangeRates/DataProviders/NetworkRepository/Router/CurrencyRouter.swift
+++ ExchangeRates/DataProviders/NetworkRepository/Router/CurrencyRouter.swift
@@ -19,7 +19,7 @@ enum CurrencyRouter {
     }
     
     func asUrlRequest() throws -> URLRequest? {
-        guard var url = URL(string: RatesApi.baseUrl) else { return nil }
+        guard let url = URL(string: RatesApi.baseUrl) else { return nil }
         
         switch self {
         case .symbols:
